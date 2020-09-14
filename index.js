002e7d8ad2a56686dbfcb76464621834aea4700a
jeonhoeun <jeonhoeun@naver.com>
2020-09-14 17:49:19 +0900

    간단한 테스트
    index.js 시작
    npm start 로 시작 가능하도록 package.json 설정.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Test")
+var express = require('express')
+var app = express()
+const PORT = 4000
+function handleListen(){
+  console.log(`Listening on : http://localhost:${PORT}`)
+}
+
+app.listen(PORT, handleListen)
+
+
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
