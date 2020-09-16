79f97f841ad9a074ef8bacadc5f99ef7107c1341
jeonhoeun <jeonhoeun@naver.com>
2020-09-16 12:37:49 +0900

    controller  추가
    controller  추가

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+import { JSONCookie } from "cookie-parser";
+
+export const join = (req,res) => res.send("Join")
+export const login = (req,res) => res.send("Login")
+export const logout = (req,res) => res.send("Logout")
