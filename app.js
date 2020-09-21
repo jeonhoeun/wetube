ec2321960617a36e662788a60e1034237540bac3
jeonhoeun <jeonhoeun@naver.com>
2020-09-21 12:17:18 +0900

    middleware를 이용하여 locals기능 추가
    middleware를 이용하여 locals기능 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet" // 보안 미들웨어
 import cookieParser from "cookie-parser" 
 import bodyParser from "body-parser"
 import routes from "./routes"
+import { localsMiddleware } from "./middlewares"
 import globalRouter from "./routers/globalRouter"
 import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
@@ -11,11 +12,13 @@ import videoRouter from "./routers/videoRouter"
 
 var app = express()
 app.set("view engine","pug")
-app.use(morgan("dev"))
+app.use(helmet())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(helmet())
+app.use(morgan("dev"))
+
+app.use(localsMiddleware);
 
 app.use(routes.home,globalRouter)
 app.use(routes.users,userRouter) 
