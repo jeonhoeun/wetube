041144e313d35e5a94082bd079cad0173ec3e64c
jeonhoeun <jeonhoeun@naver.com>
2020-09-22 12:38:09 +0900

    html 작업
    html 작업

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 #WETUBE
+
+## Pages:
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Video
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 import { JSONCookie } from "cookie-parser";
 import { reset } from "nodemon";
 
-export const join = (req,res) => res.send("Join")
-export const login = (req,res) => res.send("Login")
+export const join = (req,res) => res.render("join",{pageTitle:"Join"})
+export const login = (req,res) => res.render("Login",{pageTitle:"Login"})
 export const logout = (req,res) => res.send("Logout")
 export const users = (req,res) => res.send("users")
 export const userDetail = (req,res) => res.send("userDetail")
