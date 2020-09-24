32a1b3034a92e6f93fba9596c8de15ad0fe04bc6
jeonhoeun <jeonhoeun@naver.com>
2020-09-24 09:20:19 +0900

    CORS 오류 수정, mixins basic
    CORS 오류 수정, mixins basic

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
     title:'Video aewsome',
     description:'This is somethis I love',
     views:24,
-    videoFile:"https://archive.org/details/BigBuckBunny_124",
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator:{
       id:321,
       name:"Jeon",
@@ -16,7 +16,7 @@ export const videos = [
     title:'Video aewsome 11111',
     description:'This is somethis I love 2',
     views:12,
-    videoFile:"https://archive.org/details/BigBuckBunny_124",
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator:{
       id:321,
       name:"Jeon",
@@ -28,7 +28,7 @@ export const videos = [
     title:'Video aewsome 333',
     description:'This is somethis I love',
     views:24,
-    videoFile:"https://archive.org/details/BigBuckBunny_124",
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator:{
       id:321,
       name:"Jeon",
